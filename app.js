fba39a527c055e863d4b645d69d823774af3d11d
Tim Force <tim.w.force@gmail.com>
2015-06-02 21:55:45 -0700

    first code commit

---
+++ app.js
@@ -0,0 +1,169 @@
+
+
+var http = require('http'), 
+    url  = require('url'),
+    fs   = require('fs'),
+    path = require('path');
+var port = 3137;
+var EventEmitter = require('events').EventEmitter;
+var logger = new EventEmitter();
+var httpResponse = undefined;
+var weatherApiCredentials = undefined;
+
+
+logger.on('error', function(message) {
+    console.log("ERROR: " + message);
+});
+
+console.log("Starting ...");
+
+// lookupLocation('93101');   TODO: delete me
+
+
+function getWeatherApiCredentials(err, credentialsFile) {
+    if(err) {
+        console.log("Error finding weather api credentials. \nDone.");
+        process.exit();
+        throw err;
+    }
+
+    weatherApiCredentials = credentialsFile.toString().trim();
+    // console.log("weather api credentials = |" + weatherApiCredentials + "|");
+}
+
+
+fs.readFile('../weatherapi.txt', getWeatherApiCredentials);
+
+
+fs.readFile('./public/index.html', function(err, htmlFile) {
+  if(err) {
+    throw err;
+  }
+
+  http.createServer(function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var extName = path.extname(pathname);
+    console.log("URL parse for current request:  pathname=|" + pathname + "|   request.url=|" + request.url + "|   extname=|" + extName + "|");
+
+    // handling JavaScript files: http://ericsowell.com/blog/2011/5/6/serving-static-files-from-node-js
+    if(extName === ".js") {
+        console.log("Serving JavaScript request");
+        path.exists('./public' + pathname, function(doesExist) {
+            if(doesExist) {
+                fs.readFile('./public' + pathname, function(err, javascriptFile) {
+                    response.writeHead(200, {'Content-Type': 'text/javascript'});
+                    response.write(javascriptFile);
+                    response.end();
+                });
+            } else {
+                response.writeHead(404);
+                response.end();
+            }
+        });
+    } else if(extName === ".html") {
+        console.log("Serving html request");
+        path.exists('./public' + pathname, function(doesExist) {
+            if(doesExist) {
+                console.log("  html path exists");
+                fs.readFile('./public' + pathname, function(err, htmlFile) {
+                    console.log("  html read file: " + htmlFile);
+                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.write(htmlFile);
+                    response.end();
+                });
+            } else {
+                console.log("  html path does NOT exist");
+                response.writeHead(404);
+                response.end();
+            }
+        });
+    } else if(pathname === '/') {
+        console.log("Serving root path");
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        // response.write("Hello There!!!");
+        response.write(htmlFile);
+        response.end();
+    } else if(pathname === '/stuff') {
+        console.log("GOT HERE 4a");
+        response.writeHead(200);
+        response.write("Hello Stuff!!!");
+        response.end();
+    } else if(pathname === '/locationLookupByZip') {
+        console.log("GOT HERE 41");
+        var queryData = url.parse(request.url, true).query;
+        console.log("GOT HERE 42 - queryData=" + queryData);
+        // response.writeHead(200, {'Content-Type': 'application/json'});
+        console.log("GOT HERE 43 - queryData.l=" + queryData.l);
+        var tempForZip = zipLocationLookup(queryData.l);
+        // response.write(JSON.stringify({temp: tempForZip}));
+        // response.end();
+        httpResponse = response;
+    } else {
+        response.writeHead(404);
+        response.write("Not found");
+        response.end(); 
+    }
+  }).listen(port, function() {
+    console.log("Listening on port=" + port);
+  });
+});
+
+
+// ajax request from web page
+function zipLocationLookup(zip) {
+  console.log("zipLocationLookup - zip = " + zip);
+  var temp = lookupLocation(zip);   // call api
+  console.log("zipLocationLookup - temp = " + temp);
+  return temp;
+}
+
+
+function lookupLocation(zip) {
+    var http = require('http');
+    var magicNum = weatherApiCredentials;    // e.g. 'e81.............'
+
+    // http://api.wunderground.com/api/e812679dbe514838/conditions/q/93001.json
+    // The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+    var options = {
+        host: 'api.wunderground.com',
+        path: '/api/' + magicNum + '/conditions/q/' + zip + '.json'
+    };
+
+    var callback = function(response) {
+        var str = '';
+        console.log("Startnig http request callback");
+
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+            console.log('Response from weather lookup:\n' + str);
+            locationJson = JSON.parse(str);
+            console.log("\n\n\JSON response - current_observation=" + locationJson.current_observation);
+
+            if(httpResponse != undefined) {
+                if(locationJson != undefined && locationJson.current_observation != undefined) { 
+                    // location found
+                    console.log("\n\n\JSON response - temp=" + locationJson.current_observation.temp_f);
+                    httpResponse.writeHead(200, {'Content-Type': 'application/json'});
+                    httpResponse.write(JSON.stringify({temp: locationJson.current_observation.temp_f}));
+                    httpResponse.end();
+                } else {
+                    // location not found
+                    httpResponse.writeHead(200, {'Content-Type':'application/json'});
+                    httpResponse.write(JSON.stringify({temp: -1}));
+                    httpResponse.end();
+                }
+            }
+        });
+    }
+
+    console.log("Sending http request ...");
+    http.request(options, callback).end();
+}
+
+
+
---
+++ public/client.js
@@ -0,0 +1,68 @@
+
+// alert("*** Starting client.js ...");
+var globalList = [];   // [{location: '93101', temperature:200}, {location: '93001', temperature:-135}, {location: '91301', temperature: 157}];
+
+var app = angular.module('WhimsicalWeatherAngularApp', ['ngRoute']);
+// location
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/lookup', {
+      template: '<div id="lookup-section" ng-controller="AddWeatherReportController"><input type="text" id="citystatezip" name="citystatezip" /><button id="locationbutton" ng-click="addLocation();">GO</button><br /><br /></div>',
+      // templateUrl : 'public/test1.html',
+      controller  : 'AddWeatherReportController'
+    })
+    .when('/locations', {
+      // template: 'Testing TEST2',
+      templateUrl : 'locations.html',
+      // controller  : 'WeatherController'
+    });
+});
+
+
+app.controller('AddWeatherReportController', function($scope, $http) {
+
+  $scope.addLocation= function() {
+    $scope.data = {};
+    console.log("addLocation starting ...");
+    var location = document.getElementById('citystatezip').value;
+    document.getElementById('citystatezip').value = '';
+    console.log("location=|" + location + "|"); 
+    var responsePromise = $http.get("/locationLookupByZip?l=" + location);
+    responsePromise.success(function(data, status, headers, config) {
+        // alert("promise success");
+        $scope.myDataTemp = data.temp;
+        // alert("Found temp from server: " + $scope.myDataTemp);
+        document.getElementById("lookup-section").style.visibility = 'hidden';
+        if($scope.myDataTemp != -1) {
+            globalList.unshift({location: location, temperature: $scope.myDataTemp});
+        } else {
+            // alert("Not adding - value is -1");
+        }
+        // alert("globalList = " + JSON.stringify(globalList));
+    });
+    // "http://api.wunderground.com/api/e812679dbe514838/conditions/q/93001.json
+  }
+});
+
+
+app.controller('WeatherController', function($scope, $sce) {
+  // alert("WeatherController - Starting ...");
+
+  $scope.list = globalList;
+  $scope.helloMessage = "Check out current city weather temperatures";
+
+  $(function() {
+    $.get('/stuff', appendToList);
+
+    function appendToList(stuff) {
+      // alert("append to list - stuff = " + stuff);
+      var list = [];
+      for(var i in stuff) {
+        list.push($('<li>', {text: $sce.trustAsHtml(stuff[i]) }));
+      }
+      $('.block-list').append(list);
+    }
+  });
+});
+
+
