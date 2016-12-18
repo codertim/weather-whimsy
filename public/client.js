8623a50e6ef79079c19dae94c108bbd6986cc933
Tim Force <tim.w.force@gmail.com>
2016-12-17 19:48:21 -0800

    code cleanup

--- public/client.js
+++ public/client.js
@@ -1,5 +1,4 @@
 
-// alert("*** Starting client.js ...");
 var globalList = [];   // [{location: '93101', temperature:200}, {location: '93001', temperature:-135}, {location: '91301', temperature: 157}];
 
 var app = angular.module('WhimsicalWeatherAngularApp', ['ngRoute']);
@@ -31,24 +30,24 @@ app.controller('AddWeatherReportController', function($scope, $http) {
     console.log("location=|" + location + "|"); 
     var responsePromise = $http.get("/locationLookupByZip?l=" + location);
     responsePromise.success(function(data, status, headers, config) {
-        // alert("promise success");
+        console.log("promise success");
         $scope.myDataTemp = data.temp;
         // alert("Found temp from server: " + $scope.myDataTemp);
         /// document.getElementById("lookup-section").style.visibility = 'hidden';
         if($scope.myDataTemp != -1) {
             globalList.unshift({location: location, temperature: $scope.myDataTemp, city: data.city, state: data.state });
         } else {
-            // alert("Not adding - value is -1");
+            console.log("Not adding - value is -1");
         }
         // alert("globalList = " + JSON.stringify(globalList));
     });
-    // "http://api.wunderground.com/api/e812679dbe514838/conditions/q/93001.json
+    // "http://api.wunderground.com/api/exyz123zbcxyz128/conditions/q/93001.json
   }
 });
 
 
 app.controller('WeatherController', function($scope, $sce) {
-  // alert("WeatherController - Starting ...");
+  console.log("WeatherController - Starting ...");
 
   $scope.list = globalList;
   $scope.helloMessage = "Check out current city weather temperatures";
@@ -57,7 +56,7 @@ app.controller('WeatherController', function($scope, $sce) {
     $.get('/stuff', appendToList);
 
     function appendToList(stuff) {
-      // alert("append to list - stuff = " + stuff);
+      console.log("append to list - stuff = " + stuff);
       var list = [];
       for(var i in stuff) {
         list.push($('<li>', {text: $sce.trustAsHtml(stuff[i]) }));
